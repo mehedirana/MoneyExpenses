0b3005b8034e258c5fe089edd76a9d11ea61e686
mehedirana <mdmehedirana@gmail.com>
2022-04-26 04:54:41 +0600

    ----------without weekly , monthly filter and splash screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,14 +4,14 @@ import { useSelector } from 'react-redux';
 import { ExpenseList } from '../components/home/ExpenseList';
 import { COLORS } from '../styles.js/theme';
 
-const {height, width} = Dimensions.get('window')
+const { height, width } = Dimensions.get('window')
 
 const addImage = require('../assets/image/plus.png')
 
 
 
-const HomeScreen = ({navigation}) => {
-  const {expense} = useSelector((e)=> e.expenseList)
+const HomeScreen = ({ navigation }) => {
+  const { expense } = useSelector((e) => e.expenseList)
 
   return (
     <SafeAreaView style={styles.container}>
@@ -21,9 +21,12 @@ const HomeScreen = ({navigation}) => {
       <TouchableOpacity style={{ backgroundColor: COLORS.whitePure, flex:1,height: height /2 }}>
         <Text style={styles.expenseTxt}>Add weekly expense</Text>
       </TouchableOpacity> */}
-      <ExpenseList data={expense} />
-      <TouchableOpacity onPress={()=> navigation.navigate('AddExpense')} style={{flexDirection:'row-reverse', marginBottom:40, marginLeft:30, }}>
-        <Image source={addImage} style={{resizeMode:'contain', height:60, width:60,}}/>
+      {
+        expense?.length > 0 ? <ExpenseList data={expense} /> : <Text style={{ flex: 1, fontSize: 20, marginLeft: 20, marginTop: 20, color: COLORS.black50 }}>Add your expense</Text>
+      }
+
+      <TouchableOpacity onPress={() => navigation.navigate('AddExpense')} style={{ flexDirection: 'row-reverse', marginBottom: 40, marginLeft: 30, }}>
+        <Image source={addImage} style={{ resizeMode: 'contain', height: 60, width: 60, }} />
       </TouchableOpacity>
     </SafeAreaView>
   )
@@ -35,15 +38,15 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:COLORS.whitePure
+    backgroundColor: COLORS.whitePure
     // flexDirection:'row',
     // marginHorizontal:10
   },
-  expenseTxt:{
-    textAlign:'center',
-    fontSize:18,
-    fontWeight:"bold",
-    color:COLORS.blackSolid
+  expenseTxt: {
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: "bold",
+    color: COLORS.blackSolid
   }
 })
 
